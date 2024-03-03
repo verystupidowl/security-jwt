06699b3c5d2717433caf1a10eea1b896661bb362
Alexey <amaklov2002@gmail.com>
2024-03-04 02:07:54 +0400

    refactored

--- src/main/java/ru/tggc/SecurityJWT/config/JwtAuthenticationFilter.java
+++ src/main/java/ru/tggc/SecurityJWT/config/JwtAuthenticationFilter.java
@@ -44,7 +44,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 var authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
