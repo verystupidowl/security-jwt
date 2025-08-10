5bd135725f85cdbd33da9f23d0b20f475c95475e
verystupidowl <amaklov2002@gmail.com>
2025-08-11 00:50:40 +0400

    + файлы

---
+++ event-service/src/main/java/org/tggc/eventservice/EventServiceApplication.java
@@ -0,0 +1,15 @@
+package org.tggc.eventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class EventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServiceApplication.class, args);
+	}
+
+}
